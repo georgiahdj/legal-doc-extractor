58eb4d06f90bdff58f9706325315cb67085b6182
Georgia <georgia.hdj@gmail.com>
2026-05-29 21:39:38 +0300

    add README, remove test files

--- test_pre.py
+++
@@ -1,9 +0,0 @@
-from preprocessor import preprocess_for_llm
-from extractor import pdf_to_images
-
-images = pdf_to_images('data/simbolaio-agorapolisias-public.pdf')
-result = preprocess_for_llm(images[0])
-print("Chars:", result["total_chars"])
-print("Chunks:", result["total_chunks"])
-print("--- Chunk 1 ---")
-print(result["chunks"][0])
